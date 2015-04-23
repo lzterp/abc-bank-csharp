5b88732b6dda6a69b15a8771fe0c3872ecedcee9
lzterp <lzterp@gmail.com>
2015-04-23 18:42:19 -0400

    add common objects

--- abc-bank.Accounts.Common/Constants/AccountType.cs
+++ abc-bank.Accounts.Common/Constants/AccountType.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace abc_bank.Accounts.IService.Constants
+namespace abc_bank.Accounts.Common.Constants
 {
     public enum AccountType
     {
