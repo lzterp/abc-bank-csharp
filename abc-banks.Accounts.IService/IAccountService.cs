75f45e5e3d5cb9c747ae3ea3b73631539863f6c7
lzterp <lzterp@gmail.com>
2015-04-23 18:45:52 -0400

    Create Account Service Definition Class

---
+++ abc-banks.Accounts.IService/IAccountService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using abc_banks.Accounts.IService.Model;
+
+namespace abc_banks.Accounts.IService
+{
+    public interface IAccountService
+    {
+        IAccount CreateAccount(int AccountTypeid);
+
+    }
+}
---
+++ abc-banks.Accounts.IService/Model/IAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abc_banks.Accounts.IService.Model
+{
+    public interface IAccount
+    {
+        void Deposit(double amount);
+
+        void Withdraw(double amount);
+
+        double InterestEarned();
+
+        double sumTransactions();
+    }
+}
