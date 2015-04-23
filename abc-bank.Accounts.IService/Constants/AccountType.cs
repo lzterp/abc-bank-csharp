7f25223e9654f6216a0f4312ef97d65ccbb1de68
lzterp <lzterp@gmail.com>
2015-04-23 18:23:32 -0400

    Create Interface for Account and Account service

---
+++ abc-bank.Accounts.IService/Constants/AccountType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abc_bank.Accounts.IService.Constants
+{
+    public enum AccountType
+    {
+        CHECKING = 1,
+        SAVINGS = 2,
+        MAXI_SAVINGS = 3
+    }
+}
---
+++ abc-bank.Accounts.IService/IAccountService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using abc_bank.Accounts.IService.Model;
+
+namespace abc_bank.Accounts.IService
+{
+    public interface IAccountService
+    {
+        IAccount CreateAccount(int AccountTypeid);
+
+    }
+}
