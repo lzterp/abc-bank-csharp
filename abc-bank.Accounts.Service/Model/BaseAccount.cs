0b6968dcfce9a16da940305d78aed37f198a23f6
lzterp <lzterp@gmail.com>
2015-04-23 19:04:02 -0400

    add account service implementation

---
+++ abc-bank.Accounts.Service/Model/BaseAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using abc_bank;
+
+namespace abc_bank.Accounts.Service.Model
+{
+    public class BaseAccount
+    {
+        public double Ammount
+        {
+            get; set;
+        } 
+        public List<Transaction> transactions;
+    }
+}
