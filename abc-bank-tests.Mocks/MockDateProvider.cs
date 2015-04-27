3bf69ddffee568fc4763989361eac6e651b41391
lzterp <lzterp@gmail.com>
2015-04-27 00:34:16 -0400

    missed files

---
+++ abc-bank-tests.Mocks/MockDateProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using abc_bank.Accounts.Common;
+using abc_bank.Accounts.Common.IProviders;
+
+namespace abc_bank_tests.Mocks
+{
+    public class MockDateProvider : IDateProvider
+    {
+        private DateTime mocknow = DateTime.Now;
+
+        public void SetMockNow(DateTime newnow)
+        {
+            mocknow = newnow;
+        }
+
+        public DateTime Now()
+        {
+            return mocknow;
+        }
+    }
+}
